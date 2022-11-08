2e182d0eff36286260584fb4d42e6f874c41dd65
Ettisaf Rup <107711625+EttisafRup@users.noreply.github.com>
2022-11-08 23:38:37 +0600

    Delete palindrome.js

--- palindrome.js
+++
@@ -1,7 +0,0 @@
-function easyPalindrome(str) {
-  const reversedString = str.split("").reverse("").join("").toLowerCase()
-  return str.toLowerCase() === reversedString
-}
-console.log(easyPalindrome("ruppur"))
-
-// => The easy one in my POV :P
